2cabf1a3999b2146e0f845767b0e1af067636728
balram0608 <balram0608@gmail.com>
2020-05-22 23:22:48 +0530

    new input field added

--- Blog-App/lib/Screen/SignUp.dart
+++ Blog-App/lib/Screen/SignUp.dart
@@ -102,4 +102,34 @@ class _SignUpState extends State<SignUp> {
       ),
     );
   }
+
+  Widget inputField2(String hintText) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 20),
+      child: Container(
+          padding: EdgeInsets.only(left: 10),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(
+                color: Colors.amberAccent,
+                width: 4,
+              )),
+          child: Text("hello world")),
+    );
+  }
+
+  Widget inputField3(String hintText) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 20),
+      child: Container(
+          padding: EdgeInsets.only(left: 10),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(
+                color: Colors.amberAccent,
+                width: 4,
+              )),
+          child: Text("hello world 2")),
+    );
+  }
 }
