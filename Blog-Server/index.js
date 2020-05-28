8bf38042a5a0fa5040c1b6e9d656a6c26889cdfe
balram0608 <balram0608@gmail.com>
2020-05-28 21:34:41 +0530

    mongoDb connected

--- Blog-Server/index.js
+++ Blog-Server/index.js
@@ -1,4 +1,12 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/myapp", { useNewUrlParser: true });
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDb connected");
+});
 
 const port = process.env.port || 5000;
 const app = express();
