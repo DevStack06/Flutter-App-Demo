22feffcfc06dde57c64a6c1847bfe19ddfb019f6
balram0608 <balram0608@gmail.com>
2020-05-30 12:02:06 +0530

    mondb connected

--- Blog-Server/index.js
+++ Blog-Server/index.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/myapp", { useNewUrlParser: true });
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDb connected");
+});
+
+const port = process.env.port || 5000;
 const app = express();
 
-const Port = process.env.port || 5000;
+app.route("/").get((req, res) => res.json("hello world !"));
 
-app.route("/").get((req, res) => res.json("your first rest api 2"));
-
-app.listen(Port, () => console.log(`you server is running on port ${Port}`));
+app.listen(port, () => console.log(`welcome your listinnig at port ${port}`));
