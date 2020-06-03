b8c58cd23ae64dc187e31cba9decc2d806ce347b
balram0608 <balram0608@gmail.com>
2020-06-03 22:41:55 +0530

    login restapi created

--- Blog-Server/routes/user.js
+++ Blog-Server/routes/user.js
@@ -3,6 +3,31 @@ const User = require("../models/users.model");
 
 const router = express.Router();
 
+router.route("/:username").get((req, res) => {
+  User.findOne({ username: req.params.username }, (err, result) => {
+    if (err) res.status(500).json({ msg: err });
+    res.json({
+      data: result,
+      username: req.params.username,
+    });
+  });
+});
+
+router.route("/login").post((req, res) => {
+  User.findOne({ username: req.body.username }, (err, result) => {
+    if (err) return res.status(500).json({ msg: err });
+    if (result === null) {
+      return res.status(403).json("Username incorrect");
+    }
+    if (result.password === req.body.password) {
+      // here we implement the JWT token functionality
+      res.json("token");
+    } else {
+      res.status(403).json("password is incorrect");
+    }
+  });
+});
+
 router.route("/register").post((req, res) => {
   console.log("inside the register");
   const user = new User({
